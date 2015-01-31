2705742848c9094ec31ed6babc930b2360ec4bd3
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2015-01-31 22:14:57 +0100

    more translations. basic share and donate functions.

--- app/src/main/java/parad0x/deviceuptime/MainActivity.java
+++ app/src/main/java/parad0x/deviceuptime/MainActivity.java
@@ -1,6 +1,7 @@
 package parad0x.deviceuptime;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -12,14 +13,16 @@ import android.widget.TextView;
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 
 public class MainActivity extends ActionBarActivity {
     public final String TAG = this.getClass().getSimpleName();
     private String startDate;
+    private Date lastUpdate = null;
 
-    private final int[] conv = {31536000, 2592000, 86400, 3600, 60, 0};
+    private final int[] conv = {31536000, 2592000, 604800, 86400, 3600, 60, 0};
     private String[] name = null;
     private String[] names = null;
 
@@ -31,18 +34,21 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
-        name = new String[] {getString(R.string.time_y), getString(R.string.time_M),
-                getString(R.string.time_d), getString(R.string.time_h),
-                getString(R.string.time_m), getString(R.string.time_s)};
-        names = new String[] {getString(R.string.timep_y), getString(R.string.timep_M),
-                getString(R.string.timep_d), getString(R.string.timep_h),
-                getString(R.string.timep_m), getString(R.string.timep_s)};
+        name = new String[] {
+                getString(R.string.time_y), getString(R.string.time_M),
+                getString(R.string.time_w), getString(R.string.time_d),
+                getString(R.string.time_h), getString(R.string.time_m),
+                getString(R.string.time_s)};
+        names = new String[] {
+                getString(R.string.timep_y), getString(R.string.timep_M),
+                getString(R.string.timep_w), getString(R.string.timep_d),
+                getString(R.string.timep_h), getString(R.string.timep_m),
+                getString(R.string.timep_s)};
 
         startDate = getStartUptime();
 
-        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
-
         Thread t = new Thread() {
+            DatabaseHandler db = new DatabaseHandler(getApplicationContext());
 
             @Override
             public void run() {
@@ -59,7 +65,7 @@ public class MainActivity extends ActionBarActivity {
                                                startDate + "</font><br/><br/><br/>" +
                                                "<font color='#e1e1e1'>" + getString(R.string.msg_up) +
                                                ":</font><br/><font color='#4CAF50'>" +
-                                               getUptime() + "</font>";
+                                               getUptime(true) + "</font>";
                                 textViewUptimeTime.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
                             }
                         });
@@ -103,7 +109,30 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (id == R.id.action_share) {
+            shareIntent(getUptime(false));
+            return true;
+        }
+
+        if (id == R.id.action_donate) {
+            donate();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void donate() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TS7VETH3S63SS"));
+        startActivity(browserIntent);
+    }
+
+    private void shareIntent(String text) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "This is my uptime!"); // device name
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
     }
 
     private String getStartUptime() {
@@ -117,8 +146,13 @@ public class MainActivity extends ActionBarActivity {
         return sdf.format(cal.getTime());
     }
 
-    private String getUptime() {
-        return time2human(Double.parseDouble(getRawUptime()));
+    private Date getNow() {
+        Calendar cal = Calendar.getInstance();
+        return cal.getTime();
+    }
+
+    private String getUptime(boolean html) {
+        return time2human(Double.parseDouble(getRawUptime()), html);
     }
 
     private String getRawUptime() {
@@ -134,7 +168,7 @@ public class MainActivity extends ActionBarActivity {
         return uptime;
     }
 
-    private String time2human(double sec)
+    private String time2human(double sec, boolean html)
     {
         String time = "";
         int val = 0;
@@ -162,7 +196,10 @@ public class MainActivity extends ActionBarActivity {
                         time += name[i];
                 }
 
-                time += "<br/>";
+                if (html)
+                    time += "<br/>";
+                else
+                    time += " ";
                 sec -= conv[i] * val;
             }
         }
